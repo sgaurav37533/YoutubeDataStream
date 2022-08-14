c39f5323ddfbbe38d42c62fd4934bf0b029cb510
Gaurav Singh <azazel@Gauravs-MacBook.bbrouter>
2022-08-14 23:49:58 +0530

    new

--- auth.py
+++ auth.py
@@ -1,16 +1,7 @@
-import tweepy
+from googleapiclient.discovery import *
+import pandas as pd
+import seaborn as sns
 
-#Twitter API Auth
+api_key='AIzaSyAYVnxFmnaDQl9V3WsD0yMpfPqr8s7MFVQ'
 
-consumer_key ="R7jRA4ThpH9n0OHHgeSEK8g4I"
-consumer_secret="llrh1DdgFzC7GZe6OojXFQDJakC3OAChtlzgmT8PwhhhwGajeA"
-
-access_token ="1316120547940593671-QiDI4IRcVJ8SnQZgxg3V93qOCrdBGv"
-access_token_secret="iKVwhHpYS4xLSDxu8AYwPDNRqHIqOJoXQEAjWSG5nKawI"
-
-# authentication object 
-
-auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token,access_token_secret)
-api= tweepy.API(auth)
-
+youtube=build('youtube','v3',developerKey=api_key)
